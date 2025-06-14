410e539c0fcbb07e2329169039515e3ca6b1a01d
aryalegalprocess <aryalegalprocess@gmail.com>
2025-06-14 22:55:10 +0530

    Upd v4

--- models/contact.js
+++ models/contact.js
@@ -5,5 +5,4 @@ const contactSchema = new mongoose.Schema({
     email: { type: String, required: true },
     message: { type: String, required: true },
 }, { timestamps: true });
-
 module.exports = mongoose.model("Contact", contactSchema);
