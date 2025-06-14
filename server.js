8b2dab5e3752bac69184cd8aff4242b83f5f4489
aryalegalprocess <aryalegalprocess@gmail.com>
2025-06-14 22:47:27 +0530

    Upd v3

--- server.js
+++ server.js
@@ -12,9 +12,14 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: "https://www.aryalegalprocess.com",
-  methods: ["GET", "POST"],
-}));
+    origin: [
+      "https://www.aryalegalprocess.com",
+      "https://aryalegalprocess.com"
+    ],
+    methods: ["GET", "POST"],
+    credentials: true
+  }));
+  
 
 app.use(bodyParser.json());
 
