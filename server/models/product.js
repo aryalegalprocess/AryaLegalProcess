094cf6b18a99140a5f52ce67c944c9ae180c9a50
aryalegalprocess <aryalegalprocess@gmail.com>
2025-06-17 00:40:36 +0530

    Add admin files

---
+++ server/models/product.js
@@ -0,0 +1,15 @@
+const productSchema = new mongoose.Schema({
+  barcode: String,
+  name: String,
+  details: String,
+  weight: String,
+  quantity: String,
+  company: String,
+  description: String,
+  startDate: String,
+  endDate: String,
+  price: String,
+  image: String,  // Add image field here (string)
+});
+
+const Product = productConnection.model('Product', productSchema);
