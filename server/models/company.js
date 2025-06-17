a02e976135a8435848dbced58cb553ca4b6926d1
SowmyaSri22 <sgorrepa@gitam.in>
2025-06-18 00:11:03 +0530

    upd prolist

--- server/models/company.js
+++ server/models/company.js
@@ -2,15 +2,17 @@ module.exports = function (connection) {
   const mongoose = require('mongoose');
 
   const companySchema = new mongoose.Schema({
-    name: String,
-    startDate: String,
-    endDate: String,
-    status: String,
-    contactName: String,
-    contactNumber: String,
-    contactEmail: String,
-    contactAddress: String
-  });
+  id: String, // Add this
+  name: String,
+  startDate: String,
+  endDate: String,
+  status: String,
+  contactName: String,
+  contactNumber: String,
+  contactEmail: String,
+  contactAddress: String
+});
+
 
   return connection.model('Company', companySchema);
 };
