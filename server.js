058daae4a1c06912b9f061aadf4228c70c5aecd6
SowmyaSri22 <sgorrepa@gitam.in>
2025-06-18 23:09:27 +0530

    added images

--- server.js
+++ server.js
@@ -23,6 +23,8 @@ app.use(cors({
 }));
 app.use(express.json({ limit: '100mb' }));
 app.use(express.urlencoded({ extended: true, limit: '100mb' }));
+app.use('/images', express.static('images'));
+
 
 // ✅ Serve static files from the root directory
 app.use(express.static(path.join(__dirname)));
