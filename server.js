73f37dcd0ed5b4af4fd14e258a4debee1d95fa55
SowmyaSri22 <sgorrepa@gitam.in>
2025-06-20 16:30:55 +0530

    upd server.js back to norm

--- server.js
+++ server.js
@@ -65,8 +65,8 @@ const companyConnection = mongoose.createConnection(process.env.MONGO_URI_COMPAN
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
-const Company = require('./models/company')(companyConnection); // ✅ Correct
-
+const CompanyModel = require('./models/company')(companyConnection);
+const Company = require('./models/company');
 
 
 
