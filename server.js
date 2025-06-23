6afb1a9656412d89ba63837c89a6bdf6b25fe61d
SowmyaSri22 <sgorrepa@gitam.in>
2025-06-23 21:23:01 +0530

    upd server.js

--- server.js
+++ server.js
@@ -18,16 +18,21 @@ const allowedOrigins = [
   "http://127.0.0.1:5500"
 ];
 
-app.use((req, res, next) => {
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  }
-  next();
-});
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
+// ✅ Required to handle CORS preflight
+app.options('*', cors());
 
 
 const session = require('express-session');
